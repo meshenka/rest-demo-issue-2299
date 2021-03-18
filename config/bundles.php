ed8f0ab4b4c1d2dae1ff3d88587a3b81582af53f
Sylvain Gogel <sylvain.gogel@lydia-app.com>
2021-03-18 18:27:34 +0100

    Rest\POST vs Rest\Post

---
+++ config/bundles.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+];
---
+++ src/Controller/ServiceController.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\Response;
+
+class ServiceController extends AbstractFOSRestController
+{
+    /**
+     * @Rest\POST("/index", name="service_index")
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/ServiceController.php',
+        ]);
+    }
+
+    /**
+     * @Rest\Post("/stop", name="service_stop")
+     */
+    public function stop(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/ServiceController.php',
+        ]);
+    }
+}
