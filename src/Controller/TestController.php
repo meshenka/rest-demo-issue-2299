d1b8ae7b5f43b111c9ddd138c9733d767089ba6d
Sylvain Gogel <sylvain.gogel@lydia-app.com>
2021-03-19 10:30:35 +0100

    use exact version

---
+++ src/Controller/TestController.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\Response;
+
+class TestController extends AbstractFOSRestController
+{
+    /**
+     * @Rest\Post("/test", name="service_test")
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/ServiceController.php',
+        ]);
+    }
+}
